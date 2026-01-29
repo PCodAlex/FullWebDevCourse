d5c994d14b14ef1a4aed4fe42e3fdddc72d8ebfb
Akash Samanta <137557300+PCodAlex@users.noreply.github.com>
2026-01-30 01:48:52 +0530

    new js section about dom unlocked and added

---
+++ Js/Section14/video61/script.js
@@ -0,0 +1,8 @@
+console.log("hello developer");//hello developer
+console.log(window.outerHeight);//1180
+console.log(window.outerWidth);//820
+console.log(location.hostname);//127.0.0.1
+console.log(location.href);//http://127.0.0.1:5500/Js/Section14/video61/index.html
+console.log(navigator.userAgent);//Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.5 Safari/605.1.15 Edg/144.0.0.0
+console.log(document.firstChild);//<!Doctype html>
+console.log(document.getElementsByTagName("h1"));//HTMLCollection[h1]
