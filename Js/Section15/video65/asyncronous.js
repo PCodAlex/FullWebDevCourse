9a71a65c295b01e3485635e4193535d11e584bae
Akash Samanta <137557300+PCodAlex@users.noreply.github.com>
2026-02-02 19:22:18 +0530

    section 15 started

---
+++ Js/Section15/video65/asyncronous.js
@@ -0,0 +1,13 @@
+console.log("Akash Samanta");
+
+function sayHello(){
+    console.log("hello Sir...!");
+}
+
+setTimeout(()=>{
+    sayHello();
+},4000)
+
+for(let i=0;i<10;i++){
+    console.log(i);
+}
