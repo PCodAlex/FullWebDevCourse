1be5dda3b9a91d44489f01b4980f39681ee4a04a
Akash Samanta <137557300+PCodAlex@users.noreply.github.com>
2026-02-19 20:07:12 +0530

    project 2 added

---
+++ Js/Section17/project2/script.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    const cityInput=document.getElementById("city-input");
+    const getWeatherBtn=document.getElementById("get-weather-btn");
+    const weatherInfo=document.getElementById("weather-info");
+    const cityNameDisplay=document.getElementById("city-name");
+    const temperatureDisplay=document.getElementById("temperature");
+    const descriptionDisplay=document.getElementById("description");
+    const errorMessage=document.getElementById("error-message");
+
+    const API_KEY=`7978af40884118a5251a9b06f58c6f7e`;
+
+    getWeatherBtn.addEventListener("click",async()=>{
+        let city=cityInput.value.trim();
+        if (!city)return;
+        
+        //it may throw an error
+        //server/database is always in another continent
+
+        try {
+            const weatherData = await fetchWeatherData(city);
+            displayWeatherData(weatherData);
+        } catch (error) {
+            showError();
+        }
+
+    });
+
+    async function fetchWeatherData(city){
+        const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+
+        const response=await fetch(url);
+        console.log(response);
+
+        if(!response.ok)throw new Error("City Not Found");
+        const data=await response.json();
+        return data;
+    };
+
+    function displayWeatherData(weatherData){
+        console.log(weatherData);
+
+        const {name,main,weather}=weatherData;
+        cityNameDisplay.innerText=name;
+        temperatureDisplay.innerText=`temperature : ${main.temp}`;
+        descriptionDisplay.innerText=`weather : ${weather[0].description}`;
+
+        weatherInfo.classList.remove("hidden");
+        errorMessage.classList.add('hidden');
+
+    };
+
+    function showError(){
+        weatherInfo.classList.remove("hidden");
+        errorMessage.classList.add('hidden');
+    }
+
+})
